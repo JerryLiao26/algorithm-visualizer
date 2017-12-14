26787e2bd2a6f8cc772e0e8615eadc853e62d79d
JerryLiao26 <jerryliao26@gmail.com>
2017-12-14 09:03:59 +0800

    minor style fix

--- src/App.vue
+++ src/App.vue
@@ -40,6 +40,9 @@ export default {
   padding-left: 15px;
   padding-right: 15px;
 
+  margin-top: 15px;
+  margin-bottom: 15px;
+
   box-sizing: border-box;
   box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);
   background-color: #3f51b5;
